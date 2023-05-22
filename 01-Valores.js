9ad9d13f825fb1512f3ebefd9d19755e3c61799c
Isidoro Torrico M <itorrico.m@gmail.com>
2023-05-21 23:04:31 -0400

    Valores

---
+++ 01-Valores.js
@@ -0,0 +1,23 @@
+40
+typeof(40)
+
+"JavaScript"
+typeof("JavaScript")
+
+true
+false
+typeof(true)
+
+
+null
+typeof(null)
+
+
+undefined
+typeof(undefined)
+
+[1,2,3]
+typeof([1,2,3])
+
+{nombre: "Diego"}
+typeof({nombre: "Diego"})
