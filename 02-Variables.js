ea0b753af85de55d9c053d43949d0e8c6a280c9b
Isidoro Torrico M <itorrico.m@gmail.com>
2023-05-22 05:10:38 -0400

    Variables

---
+++ 02-Variables.js
@@ -0,0 +1,18 @@
+var nombre = "Jairo"
+
+//Declar una variable
+var edad;
+
+//inicializar
+edad = 18;
+
+//array o listas
+var elementos = ["Ram", "SSD", "HDD","Case"];
+
+
+//objeto
+var persona = {
+    nombre: "Jhoan",
+    edad: 20
+}
+
